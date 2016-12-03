644ec1280b4ac30490b0b59a9b2dda7f36891b4d
Tiffany Chan <tiffany.chan@queensu.ca>
2016-12-03 09:58:32 -0500

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,14 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Controller {
+
+    @FXML
+    private Button enterButton;
+    
+    void handleEnterButton(ActionEvent event) { 
+    	
+    	
+    }//end handleEnterButton()
+}//end Controller 
